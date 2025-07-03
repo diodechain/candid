d6f132c6e18459242a39f0295c2cb008c10a57b9
Dominic Letz <dominic@diode.io>
2025-07-03 08:43:31 +0200

    v1.3.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Candid.MixProject do
   def project do
     [
       app: :candid,
-      version: "1.3.0",
+      version: "1.3.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
